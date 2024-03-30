726a35003f21a739fc1a0f02f32265dd572cad58
Mattia <82611659+Xhst@users.noreply.github.com>
2024-03-30 11:38:39 +0100

    feat: load_r1 PL/pgSQL

---
+++ project-1/load_r1.sql
@@ -0,0 +1,18 @@
+set search_path to "tagd";
+delete from r1;
+
+create or replace function load_r1(table_size integer, b_max integer, c_max integer) 
+    returns void as $$
+begin
+    for i in 1..table_size loop
+        insert into r1 (a,b,c)
+            values(
+                i, 
+                floor(random()*b_max+1),
+                floor(random()*c_max)+1 
+            );
+    end loop;
+end;
+
+$$ language plpgsql;
+select load_r1(1000000, 100, 2000000);
