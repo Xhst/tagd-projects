a6f6b046c543e5ed9d342b49700d3789d783a757
Mattia <82611659+Xhst@users.noreply.github.com>
2024-03-30 11:38:54 +0100

    feat: load_r3 PL/pgSQL

---
+++ project-1/load_r3.sql
@@ -0,0 +1,18 @@
+set search_path to "tagd";
+delete from r3;
+
+create or replace function load_r3(table_size integer, h_max integer, i_max integer) 
+    returns void as $$
+begin
+    for i in 1..table_size loop
+        insert into r3 (g,h,i)
+            values(
+                i, 
+                floor(random()*h_max+1),
+                floor(random()*i_max)+1 
+            );
+    end loop;
+end;
+
+$$ language plpgsql;
+select load_r3(300000, 5000, 500);
