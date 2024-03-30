e90a28106cd673ecfabf4f81aed42d863c40cd01
Mattia <82611659+Xhst@users.noreply.github.com>
2024-03-30 11:34:07 +0100

    feat: schema and tables

---
+++ project-1/prepare.sql
@@ -0,0 +1,23 @@
+---drop schema "tagd" cascade;
+create schema "tagd";
+
+set search_path to "tagd";
+
+/* 
+Create the tables r1(a,b,c) r2(d,e,f) r3(g,h,i)
+all the fields are numeric with a, b and g as primary keys
+*/
+drop table if exists r1;
+create table r1 ( a numeric not null primary key, b numeric, c numeric);
+
+drop table if exists r2;
+create table r2 ( d numeric not null primary key, e numeric, f numeric);
+
+drop table if exists r3;
+create table r3 ( g numeric not null primary key, h numeric, i numeric);
+
+/*
+Constraints
+*/
+alter table r1 add constraint fk1 foreign key (c) references r2 (d);
+alter table r2 add constraint fk2 foreign key (f) references r3 (g);
