37ac03d498dae02abfa901839886cb098cd70583
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-02 18:23:32 +0200

    style: removed spaces

--- project-1/init-db/001-prepare.sql
+++ project-1/init-db/001-prepare.sql
@@ -3,13 +3,13 @@ Create the tables r1(a,b,c) r2(d,e,f) r3(g,h,i)
 all the fields are numeric with a, b and g as primary keys
 */
 drop table if exists r1;
-create table r1 ( a numeric not null primary key, b numeric, c numeric);
+create table r1 (a numeric not null primary key, b numeric, c numeric);
 
 drop table if exists r2;
-create table r2 ( d numeric not null primary key, e numeric, f numeric);
+create table r2 (d numeric not null primary key, e numeric, f numeric);
 
 drop table if exists r3;
-create table r3 ( g numeric not null primary key, h numeric, i numeric);
+create table r3 (g numeric not null primary key, h numeric, i numeric);
 
 /*
 Constraints
