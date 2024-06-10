20b2c1fa3fe00e159f3c79e0b59e13cf7be98a5a
Carlo <car.proserpio@stud.uniroma3.it>
2024-06-10 08:43:22 +0200

    feat: modified postgres config (track_io_timing, max_parallel_workers, max_worker_processes)

--- project-3/init-db/postgres-config.sh
+++ project-3/init-db/postgres-config.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Define the file to be modified
+# SHARED PRELOAD LIB
 FILE="/var/lib/postgresql/data/postgresql.conf"
 
-# Use sed to find and replace the line
 sed -i.bak "s/#shared_preload_libraries = ''/shared_preload_libraries = 'pg_stat_statements'/" "$FILE"
-
+sed -i.bak "s/#max_worker_processes = 8/max_worker_processes = 0/" "$FILE"
+sed -i.bak "s/#max_parallel_workers = 8/max_parallel_workers = 0/" "$FILE"
+sed -i.bak "s/#track_io_timing = off/track_io_timing = on/" "$FILE"
