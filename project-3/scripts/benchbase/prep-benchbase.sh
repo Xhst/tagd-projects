77746ddc3397fb0a4e2b0ab10bf3e0b7bc4d6b55
Carlo <car.proserpio@stud.uniroma3.it>
2024-06-10 11:08:24 +0200

    feat: prepare banchbase and set scale factor

---
+++ project-3/scripts/benchbase/prep-benchbase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# extract the benchmarking archive
+cd /root/benchbase-2023/target
+tar xvzf benchbase-postgres.tgz
+
+# change scale factor
+FILE="/root/benchbase-2023/target/benchbase-postgres/config/postgres/sample_tpch_config.xml"
+SCALE_FACTOR=5
+cd benchbase-postgres/config/postgres
+sed -i.bak "s|<scalefactor>0.1</scalefactor>|<scalefactor>$SCALE_FACTOR</scalefactor>|" "$FILE"
