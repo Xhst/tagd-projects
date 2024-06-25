8ca7e87ccd008ed5b2720508d4ae319e32ae103f
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-25 12:39:20 +0200

    feat: added db persistency

--- project-3/scripts/benchbase/prep-benchbase.sh
+++ project-3/scripts/benchbase/prep-benchbase.sh
@@ -6,6 +6,6 @@ tar xvzf benchbase-postgres.tgz
 
 # change scale factor
 FILE="/root/benchbase-2023/target/benchbase-postgres/config/postgres/sample_tpch_config.xml"
-SCALE_FACTOR=5
+SCALE_FACTOR=2
 cd benchbase-postgres/config/postgres
 sed -i.bak "s|<scalefactor>0.1</scalefactor>|<scalefactor>$SCALE_FACTOR</scalefactor>|" "$FILE"
