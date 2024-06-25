9aacb872f432b00e4faa4a0c208dd35e5e145587
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-25 15:53:33 +0200

    comment

--- project-3/scripts/benchbase/prep-benchbase.sh
+++ project-3/scripts/benchbase/prep-benchbase.sh
@@ -4,8 +4,10 @@
 cd /root/benchbase-2023/target
 tar xvzf benchbase-postgres.tgz
 
-# move query files to the correct directory
+# move query files and copy custom samples to the correct directory
 mv /root/benchbase-2023/queries /root/benchbase-2023/target/benchbase-postgres
+cp /root/samples/* /root/benchbase-2023/target/benchbase-postgres/config/postgres/
+
 
 # change scale factor
 FILE="/root/benchbase-2023/target/benchbase-postgres/config/postgres/sample_tpch_config.xml"
