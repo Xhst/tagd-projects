edc024fc6622f286b9b181846291f7fffa7007b9
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-06-29 11:28:05 +0200

    update script

--- project-3/scripts/python/mean_iostat.py
+++ project-3/scripts/python/mean_iostat.py
@@ -20,13 +20,13 @@ def calculate_averages(data):
     min_user = min(user_values) if user_values else 0.0
 
     # Find %util values for sdd
-    sdd_section_pattern = re.compile(r"sdd\s+[\d. ]+\s+(\d+\.\d+)")
-    util_values = [float(value) for value in sdd_section_pattern.findall(data)]
-    avg_util_sdd = sum(util_values) / len(util_values) if util_values else 0.0
-    max_util_sdd = max(util_values) if util_values else 0.0
-    min_util_sdd = min(util_values) if util_values else 0.0
+    sde_section_pattern = re.compile(r"sde\s+[\d. ]+\s+(\d+\.\d+)")
+    util_values = [float(value) for value in sde_section_pattern.findall(data)]
+    avg_util_sde = sum(util_values) / len(util_values) if util_values else 0.0
+    max_util_sde = max(util_values) if util_values else 0.0
+    min_util_sde = min(util_values) if util_values else 0.0
 
-    return avg_user, max_user, min_user, avg_util_sdd, max_util_sdd, min_util_sdd
+    return avg_user, max_user, min_user, avg_util_sde, max_util_sde, min_util_sde
 
 # Main script
 if __name__ == "__main__":
@@ -36,13 +36,13 @@ if __name__ == "__main__":
 
     filename = sys.argv[1]
     data = read_data_from_file(filename)
-    avg_user, max_user, min_user, avg_util_sdd, max_util_sdd, min_util_sdd = calculate_averages(data)
+    avg_user, max_user, min_user, avg_util_sde, max_util_sde, min_util_sde = calculate_averages(data)
 
     print("CPU Utilization:")
     print(f"\tAverage %user: {avg_user:.2f}")
     print(f"\tMaximum %user: {max_user:.2f}")
     print(f"\tMinimum %user: {min_user:.2f}")
     print("Disk Utilization:")
-    print(f"\tAverage %util for sdd: {avg_util_sdd:.2f}")
-    print(f"\tMaximum %util for sdd: {max_util_sdd:.2f}")
-    print(f"\tMinimum %util for sdd: {min_util_sdd:.2f}")
+    print(f"\tAverage %util for disk: {avg_util_sde:.2f}")
+    print(f"\tMaximum %util for disk: {max_util_sde:.2f}")
+    print(f"\tMinimum %util for disk: {min_util_sde:.2f}")
